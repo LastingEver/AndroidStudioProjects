f38f0f401d4216670226dcfa81a2380c647f30f9
LuJia <UESTC.SISE.Lasting.Ever@gmail.com>
2016-10-20 13:07:21 +0800

    fix bugs

--- TravelAssistant/app/src/main/java/lasting/travelassistant/MapPage.java
+++ TravelAssistant/app/src/main/java/lasting/travelassistant/MapPage.java
@@ -3,10 +3,10 @@ package lasting.travelassistant;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.SearchView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import com.amap.api.location.AMapLocation;
@@ -20,9 +20,19 @@ import com.amap.api.maps.SupportMapFragment;
 import com.amap.api.maps.UiSettings;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.MarkerOptions;
+import com.amap.api.maps.overlay.PoiOverlay;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.core.SuggestionCity;
+import com.amap.api.services.help.Inputtips;
+import com.amap.api.services.help.InputtipsQuery;
+import com.amap.api.services.help.Tip;
+import com.amap.api.services.poisearch.PoiResult;
+import com.amap.api.services.poisearch.PoiSearch;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class MapPage extends SupportMapFragment implements LocationSource, AMapLocationListener {
     private AMap aMap = null;
@@ -38,6 +48,20 @@ public class MapPage extends SupportMapFragment implements LocationSource, AMapL
 
     private SearchView sv = null;
 
+    private PoiSearch ps = null;
+
+    private PoiSearch.Query query = null;
+
+    private String currentCity = null;
+
+    private int currentPage = 0;
+
+    private PoiOverlay po = null;
+
+    private SearchView.SearchAutoComplete sac = null;
+
+    private List<String> tipList = null;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -52,23 +76,97 @@ public class MapPage extends SupportMapFragment implements LocationSource, AMapL
         return view;
     }
 
+    private void initPoi() {
+        query = new PoiSearch.Query(sv.getQuery().toString(), "", currentCity);
+        query.setPageSize(10);
+        query.setPageNum(currentPage);
+
+        ps = new PoiSearch(this.getActivity(), query);
+        ps.setOnPoiSearchListener(new PoiSearch.OnPoiSearchListener() {
+            @Override
+            public void onPoiSearched(PoiResult poiResult, int i) {
+                if (i == 1000) {
+                    if (poiResult != null && poiResult.getQuery() != null) {
+                        if (poiResult.getQuery().equals(query)) {
+                            List<PoiItem> poiItems = poiResult.getPois();
+                            List<SuggestionCity> suggestionCitis = poiResult.getSearchSuggestionCitys();
+
+                            if (poiItems != null && poiItems.size() > 0) {
+                                aMap.clear();
+                                po = new PoiOverlay(aMap, poiItems);
+                                po.removeFromMap();
+                                po.addToMap();
+                                po.zoomToSpan();
+                            } else if (suggestionCitis != null && suggestionCitis.size() > 0) {
+                                Toast.makeText(getActivity(), "推荐城市：" + suggestionCitis.get(0).getCityName(), Toast.LENGTH_SHORT);
+                            } else {
+                                Toast.makeText(getActivity(), "没有搜索到数据", Toast.LENGTH_SHORT);
+                            }
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onPoiItemSearched(PoiItem poiItem, int i) {
+
+            }
+        });
+        ps.searchPOIAsyn();
+    }
+
     private void initSearch() {
         sv = (SearchView) view.findViewById(R.id.searchView);
+        sac = (SearchView.SearchAutoComplete) sv.findViewById(R.id.search_src_text);
         sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
+                initPoi();
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
+                if (newText != null && !newText.isEmpty()) {
+                    InputtipsQuery iq = new InputtipsQuery(newText, currentCity);
+                    Inputtips inputtips = new Inputtips(getActivity(), iq);
+                    inputtips.setInputtipsListener(new Inputtips.InputtipsListener() {
+                        @Override
+                        public void onGetInputtips(List<Tip> list, int i) {
+                            if (i == 1000) {
+                                tipList = new ArrayList<String>();
+                                for (Tip tip : list) {
+                                    tipList.add(tip.getName());
+                                }
+                                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, tipList);
+                                sac.setAdapter(adapter);
+                                adapter.notifyDataSetChanged();
+                            } else {
+
+                            }
+                        }
+                    });
+                    inputtips.requestInputtipsAsyn();
+                }
                 return false;
+            }
+        });
+        sv.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
+            @Override
+            public boolean onSuggestionSelect(int position) {
+                return false;
+            }
+
+            @Override
+            public boolean onSuggestionClick(int position) {
+                sac.setText(tipList.get(position));
+                return true;
             }
         });
     }
 
     private void initMap() {
-        aMap = ((SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map)).getMap();
+        aMap = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map)).getMap();
         aMap.setTrafficEnabled(true);
         aMap.setLocationSource(this);
 
@@ -100,7 +198,7 @@ public class MapPage extends SupportMapFragment implements LocationSource, AMapL
     @Override
     public void activate(LocationSource.OnLocationChangedListener onLocationChangedListener) {
         this.onLocationChangedListener = onLocationChangedListener;
-        if (aMapLocationClient == null){
+        if (aMapLocationClient == null) {
             initLoc();
         }
     }
@@ -108,7 +206,7 @@ public class MapPage extends SupportMapFragment implements LocationSource, AMapL
     @Override
     public void deactivate() {
         this.onLocationChangedListener = null;
-        if (aMapLocationClient != null){
+        if (aMapLocationClient != null) {
             aMapLocationClient.stopLocation();
             aMapLocationClient.onDestroy();
             aMapLocationClient = null;
@@ -117,8 +215,8 @@ public class MapPage extends SupportMapFragment implements LocationSource, AMapL
 
     @Override
     public void onLocationChanged(AMapLocation aMapLocation) {
-        if (aMapLocation != null){
-            if (aMapLocation.getErrorCode() == 0){
+        if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
                 aMapLocation.getLocationType();
                 aMapLocation.getLatitude();
                 aMapLocation.getLongitude();
@@ -131,14 +229,14 @@ public class MapPage extends SupportMapFragment implements LocationSource, AMapL
                 aMapLocation.getAddress();
                 aMapLocation.getCountry();
                 aMapLocation.getProvince();
-                aMapLocation.getCity();
+                currentCity = aMapLocation.getCity();
                 aMapLocation.getDistrict();
                 aMapLocation.getStreet();
                 aMapLocation.getStreetNum();
                 aMapLocation.getCityCode();
                 aMapLocation.getAdCode();
 
-                if (isFirstLoc){
+                if (isFirstLoc) {
                     aMap.moveCamera(CameraUpdateFactory.zoomTo(17));
                     aMap.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude())));
                     onLocationChangedListener.onLocationChanged(aMapLocation);
@@ -152,7 +250,6 @@ public class MapPage extends SupportMapFragment implements LocationSource, AMapL
                     isFirstLoc = false;
                 }
             } else {
-                Log.e("AMapError", "Location Error, ErrCode:" + aMapLocation.getErrorCode() + ", errInfo:" + aMapLocation.getErrorInfo());
                 Toast.makeText(this.getActivity(), "定位失败", Toast.LENGTH_SHORT).show();
             }
         }
@@ -162,8 +259,8 @@ public class MapPage extends SupportMapFragment implements LocationSource, AMapL
         MarkerOptions mo = new MarkerOptions();
         mo.position(new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude()));
         StringBuffer sb = new StringBuffer();
-        sb.append(aMapLocation.getCountry() + "" + aMapLocation.getProvince() + "" + aMapLocation.getCity() +  "" + aMapLocation.getDistrict() + "" + aMapLocation.getStreet() + "" + aMapLocation.getStreetNum());
+        sb.append(aMapLocation.getCountry() + "" + aMapLocation.getProvince() + "" + aMapLocation.getCity() + "" + aMapLocation.getDistrict() + "" + aMapLocation.getStreet() + "" + aMapLocation.getStreetNum());
         mo.title(sb.toString());
-        return  mo;
+        return mo;
     }
 }
